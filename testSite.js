bf9469cfe6d51ea63b144ac20288ae589f748c12
krimsonduke <griffen.ryan.angel@gmail.com>
2026-09-10 15:04:15 -0400

    Made the message a little more rude.

--- testSite.js
+++ testSite.js
@@ -14,6 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
         heading.style.color = "#28a745"; 
 
         // Pop up a quick alert browser message
-        alert("Success! Your JavaScript code is working.");
+        alert("Don't be a jerk. Push the damn button.");
     });
 });
